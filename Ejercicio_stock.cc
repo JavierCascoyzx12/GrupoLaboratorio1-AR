8a0d6b6f5e8e487901b2e18d9bc4ae3d1892006b
JavierCascoyzx12 <00102326@uca.edu.sv>
2026-09-10 12:37:53 -0600

    feat: implementar incremento de stock mediante puntero por referencia

--- Ejercicio_stock.cc
+++ Ejercicio_stock.cc
@@ -1,20 +1,26 @@
 #include <iostream>
 
-int stock;
+int stock = 50;
 
 void IngresarStock(int &stock, int cantidad);
 
+
 int main(){
 
-
-    return 0;
+    int cantidad;
+    std::cout<<"ingrese el nuevo stock:"<< std::endl;
+    std::cin>>cantidad;
+    if(cantidad < 0){
+        std::cout<<"el stock no puede ser negativo"<<std::endl;
+        return 1;
+    } else{
+        IngresarStock(stock, cantidad);
+    std::cout<<"el stock actual es: "<<stock<<std::endl;
+    }
+    
 }
 
 void IngresarStock(int &stock, int cantidad){
-
-    std::cout<<"ingrese la cantidad de stock:"<< endl;
-    std::cin>>cantidad;
-
     stock += cantidad;
 }
 
